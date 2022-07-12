fb5801274f2a23acea64bb98425a1e447f4bf1d9
Vladimir <vmolesov@gmail.com>
2022-07-12 13:52:26 +0500

    Dao layer removed, initialization block created in service

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,32 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDao;
-import web.model.Car;
-import web.service.CarService;
-import web.service.CarServiceImp;
 
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarService;
 
 @Controller
 public class CarController {
 
-    private CarDao carDao;
+    private CarService carService;
 
-    public CarController(CarDao carDao) {
-        this.carDao = carDao;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping(value = "/cars")
     public String printCars(ModelMap model, @RequestParam(defaultValue = "5") int count) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("Model1", "BodyType1", 2));
-        cars.add(new Car("Model2", "BodyType2", 3));
-        cars.add(new Car("Model3", "BodyType3", 4));
-        cars.add(new Car("Model4", "BodyType4", 5));
-        cars.add(new Car("Model5", "BodyType5", 5));
-        model.addAttribute("cars", carDao.getListCars(count, cars));
+        model.addAttribute("cars", carService.getListCars(count));
         return "cars";
     }
 
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,25 +1,33 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.dao.CarDao;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarServiceImp implements CarService {
 
+    private List<Car> cars = new ArrayList<>();
 
-    private CarDao carDao;
-
-    @Autowired
-    public CarServiceImp(CarDao carDao) {
-        this.carDao = carDao;
+    {
+        cars.add(new Car("Model1", "BodyType1", 2));
+        cars.add(new Car("Model2", "BodyType2", 3));
+        cars.add(new Car("Model3", "BodyType3", 4));
+        cars.add(new Car("Model4", "BodyType4", 5));
+        cars.add(new Car("Model5", "BodyType5", 5));
     }
 
     @Override
-    public List<Car> getListCars(int count, List<Car> listCars) {
-        return carDao.getListCars(count, listCars);
+    public List<Car> getListCars(int count) {
+        List<Car> getList = new ArrayList<>();
+        if (count > 5) {
+            count = 5;
+        }
+        for (int i = 0; i < count; i++) {
+            getList.add(cars.get(i));
+        }
+        return getList;
     }
 }
